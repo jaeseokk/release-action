f2d75ebcb7a4dae96798573067b960f8a0bc528f
jaeseokk <kang89kr@gmail.com>
2021-07-01 17:42:36 +0900

    Fix bump commit author name

--- src/utils/git.ts
+++ src/utils/git.ts
@@ -2,12 +2,12 @@ import {exec} from '../utils/exec';
 import {GitTag, Package} from '../types';
 
 export const setupUser = async () => {
-  await exec('git', ['config', '--global', 'user.name', `"github-actions[bot]"`]);
+  await exec('git', ['config', '--global', 'user.name', `"hb-release[bot]"`]);
   await exec('git', [
     'config',
     '--global',
     'user.email',
-    `"github-actions[bot]@users.noreply.github.com"`,
+    `"hb-release[bot]@users.noreply.github.com"`,
   ]);
 };
 
